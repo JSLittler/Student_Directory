6ee21f7c1d45006d1e8d9ffc6775dce01a276d86
John Littler <johnlittler@bgl-mac-008785.bgl.financial.local>
2018-09-20 14:58:43 +0100

    6th Commit - more 'directory.rb' re-factoring.

--- directory.rb
+++ directory.rb
@@ -1,17 +1,18 @@
-student_count = 11
+students = [
+  "The students of Villains Academy",
+  "_____________",
+  "Dr. Hannibal Lecter",
+  "Darth Vader", 
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
+puts students
 
-puts "The students of Villains Academy"
-puts "_____________"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-
-print "Overall, we have #{student_count} great students"
+puts "Overall, we have #{students.count} great students"
