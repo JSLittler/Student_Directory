d66c3e972d6dd02be63d6a3f77ffd506bc6111d9
John Littler <johnlittler@bgl-mac-008785.bgl.financial.local>
2018-09-20 15:07:26 +0100

    7th commit - Even more 'directory.rb' refactoring. This time moved into methods.

--- directory.rb
+++ directory.rb
@@ -1,6 +1,4 @@
 students = [
-  "The students of Villains Academy",
-  "_____________",
   "Dr. Hannibal Lecter",
   "Darth Vader", 
   "Nurse Ratched",
@@ -13,6 +11,22 @@ students = [
   "Joffrey Baratheon",
   "Norman Bates"
 ]
-puts students
 
-puts "Overall, we have #{students.count} great students"
+def print_header
+  puts "The students of Villains Academy"
+  puts "_____________"
+end
+
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
