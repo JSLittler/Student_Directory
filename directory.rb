a48fbfa3cb31d6d7f1c12fe2d31704cd9f017bb4
John Littler <johnlittler@bgl-mac-008785.bgl.financial.local>
2018-09-24 10:46:06 +0100

    Commit 13 - Save and load students functionality entered. student csv file moved to .gitignore.

--- directory.rb
+++ directory.rb
@@ -33,6 +33,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit" 
 end
 
@@ -45,12 +46,22 @@ end
 def save_students
   file = File.open("students.csv", "w")
   @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
+    student_data = [ID[:@students.length + 1], @student[:name], @student[:cohort]]
     csv_line = student_data.join(",")
     file.puts csv_line
   end
   file.close
   puts "Student details have been saved"
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    @students << {ID: @students.length + 1, name: name, cohort: cohort.to_sym}
+  end
+  file.close
+  puts "Previously saved student details have now been added"
 end
 
 def process(selection)
@@ -61,6 +72,8 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit
     else
