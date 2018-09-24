7ef95058a8175ac61fac2d224acb652955cfb21f
John Littler <johnlittler@bgl-mac-008785.bgl.financial.local>
2018-09-24 09:15:02 +0100

    Commit 9: Interactive menu 1, 3 option menu.

--- directory.rb
+++ directory.rb
@@ -1,35 +1,54 @@
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
-
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.chomp
+  while !name.empty? do
+    students << {ID: students.length + 1, name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "_____________"
+  puts "The students of Villains Academy".center(100)
+  puts "_____________".center(100)
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[0]} (#{student[1]} cohort)"
+  n = 1
+  students.each_with_index do |student|
+    puts "#{student[:ID]}. #{student[:name]} (#{student[:cohort]} cohort).".ljust(100)
+    n += 1
   end
 end
 
-def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students".ljust(100)
+end
+
+def interactive_menu
+  students = []
+  loop do
+    puts "What would you like to do?"
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+      when "1"
+        students = input_students
+      when "2"
+        print_header
+        print(students)
+        print_footer(students)
+      when "9"
+        exit
+      else
+        puts "I don't know what you meant, try again"
+    end
   end
 end
 
-print_header
-print(students)
-print_footer(students)
+interactive_menu
