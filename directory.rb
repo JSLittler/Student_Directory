ef16566ccd4d8dfd735a16793e752a9d8b5864e0
John Littler <johnlittler@bgl-mac-008785.bgl.financial.local>
2018-09-24 12:53:19 +0100

    Completed student directory

--- directory.rb
+++ directory.rb
@@ -1,15 +1,47 @@
 @students = []
+filename = "students.csv"
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
+  puts "9. Exit" 
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)
+  end
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "3"
+      save_students
+    when "4"
+      load_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, try again"
+  end
+end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  name = gets.chomp
+  name = STDIN.gets.chomp
   while !name.empty? do
     @students << {id: @students.length + 1, name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
-  @students
 end
 
 def print_header
@@ -30,14 +62,6 @@ def print_footer
   puts "Overall, we have #{@students.count} great students".center(100)
 end
 
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
-  puts "9. Exit" 
-end
-
 def show_students
   print_header
   print_student_list
@@ -55,8 +79,8 @@ def save_students
   puts "Student details have been saved."
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
   id, name, cohort = line.chomp.split(',')
     @students << {id: id, name: name, cohort: cohort.to_sym}
@@ -65,28 +89,17 @@ def load_students
   puts "Previously saved student details have now been added"
 end
 
-def process(selection)
-  case selection
-    when "1"
-      input_students
-    when "2"
-      show_students
-    when "3"
-      save_students
-    when "4"
-      load_students
-    when "9"
-      exit
-    else
-      puts "I don't know what you mean, try again"
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+     puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist."
+    exit
   end
 end
 
-def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)
-  end
-end
-
+try_load_students
 interactive_menu
